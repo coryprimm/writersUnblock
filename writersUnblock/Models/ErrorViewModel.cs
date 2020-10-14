53401867047696dd0c6e8233f67180d07c034fe0
Cory Primm <corypr1mm@yahoo.com>
2020-10-13 17:32:29 -0700

    Initial check-in of module writersUnblock

---
+++ writersUnblock/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace writersUnblock.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
